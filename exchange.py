8ce4b720bc163576b241339daeee88369d7eaa7b
theharms <117523637+theharms@users.noreply.github.com>
2024-11-04 18:36:05 +0300

    добавлена функция exchange получения курсов валют

--- exchange.py
+++ exchange.py
@@ -1,11 +1,25 @@
-from importlib.metadata import entry_points
-
 import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
 
+def exchange():
+    code = entry.get()
 
+    if code:
+        try:
+            response = requests.get("https://open.er-api.com/v6/latest/USD")
+            response.raise_for_status()
+            data = response.json()
+            if code in data["rates"]:
+                exchange_rate = data["rates"][code]
+                mb.showinfo("exchange rate",f"exchange {exchange_rate} for 1 USD")
+            else:
+                mb.showerror("Error", f"currency {code} not found")
+        except Exception as e:
+            mb.showerror("Error", f"Error: {e}")
+    else:
+        mb.showwarning("Warning", "Enter currency code")
 
 window = Tk()
 window.title("exchange rate")
