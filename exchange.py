0e67567fcc0d49473d756b2f66013b1b9dea42f0
theharms <117523637+theharms@users.noreply.github.com>
2024-11-05 18:04:17 +0300

    вместо поля ввода разместили выпадающий список валют

--- exchange.py
+++ exchange.py
@@ -1,10 +1,12 @@
+from tkinter.ttk import Combobox
+
 import requests
 import json
 from tkinter import *
 from tkinter import messagebox as mb
-
+from tkinter import ttk
 def exchange():
-    code = entry.get()
+    code = combobox.get()
 
     if code:
         try:
@@ -13,7 +15,7 @@ def exchange():
             data = response.json()
             if code in data["rates"]:
                 exchange_rate = data["rates"][code]
-                mb.showinfo("exchange rate",f"exchange {exchange_rate} for 1 USD")
+                mb.showinfo("exchange rate",f"exchange {exchange_rate:.2f} for 1 USD")
             else:
                 mb.showerror("Error", f"currency {code} not found")
         except Exception as e:
@@ -25,10 +27,13 @@ window = Tk()
 window.title("exchange rate")
 window.geometry("360x180")
 
-Label(text="Enter currency code").pack(padx=10, pady=10)
+Label(text="choose currency code").pack(padx=10, pady=10)
+cur = ["RUB","EUR", "GBP", "JPY", "CNY", "KZT", "UZS", "CHF", "AED", "CAD"]
+combobox = ttk.Combobox(values=cur)
+combobox.pack(padx=10, pady=10)
 
-entry = Entry()
-entry.pack(padx=10, pady=10)
+# entry = Entry()
+# entry.pack(padx=10, pady=10)
 
 Button(text="get exchange", command=exchange).pack(padx=10, pady=10)
 
